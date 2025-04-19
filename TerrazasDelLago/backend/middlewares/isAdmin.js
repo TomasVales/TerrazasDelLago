4518bb8880a137d5493cf8a380bf9f67b38e8c44
Ignacio Vargas Cancelliere <nacho.vargas0208@gmail.com>
2025-04-19 17:02:09 -0300

    Delete TerrazasDelLago directory

--- TerrazasDelLago/backend/middlewares/isAdmin.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-        next(); // continúa a la ruta
-    } else {
-        res.status(403).json({ message: 'Acceso denegado. Solo para administradores.' });
-    }
-};
-
-module.exports = isAdmin;
--- TerrazasDelLago/backend/routes/statsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statsController = require('../controllers/statsController');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/', authMiddleware.verifyToken, authMiddleware.isAdmin, statsController.getStats);
-
-module.exports = router;
-
